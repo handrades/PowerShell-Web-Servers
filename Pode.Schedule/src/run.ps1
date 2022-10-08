b0b61768343f57da1a59d8af3bd1597c00ab726c
Héctor Andrade <andrad_h@hotmail.com>
2022-10-08 17:57:06 -0500

    adds schedule functionality

---
+++ Pode.Schedule/src/run.ps1
@@ -0,0 +1,31 @@
+Param(
+    [int]$port = (Get-ChildItem env:/HTTP_PORT).value
+)
+
+Import-Module Pode
+
+Start-PodeServer {
+
+    Add-PodeEndpoint -Address * -Port ($port ? $port : 8080) -Protocol Https -SelfSigned
+
+    New-PodeLoggingMethod -Terminal | Enable-PodeErrorLogging -Levels Error,Warning,Informational
+
+    Add-PodeSchedule -Name EveryMin -Cron '*/1 * * * *' -ScriptBlock {
+        Write-Host "Forever every minute - $(Get-Date)"
+    }
+
+    Add-PodeSchedule -Name twice -Cron '*/1 * * * *' -Limit 2 -ScriptBlock {
+        Write-Host "Run every minute only 2 times - $(Get-Date)"
+    }
+
+    $start = [DateTime]::Now.AddSeconds(15)
+    Add-PodeSchedule -Name StartAT -Cron '*/1 * * * *' -StartTime $start -ScriptBlock {
+        Write-Host "Delay start 15 seconds run every minute afterwards - $(Get-Date)"
+    }
+
+    $end = [DateTime]::Now.AddSeconds(181)
+    Add-PodeSchedule -Name EndAt -Cron '*/1 * * * *' -EndTime $end -ScriptBlock {
+        Write-Host "Define end in 181 seconds, run every minute - $(Get-Date)"
+    }
+
+}
