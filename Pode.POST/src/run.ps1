ba9846c931ae68e21f6dfc409f75a334420732bf
Héctor Andrade <andrad_h@hotmail.com>
2022-10-08 13:20:38 -0500

    adds a working POST endpoint

--- Pode.POST/src/run.ps1
+++ Pode.POST/src/run.ps1
@@ -11,10 +11,21 @@ Start-PodeServer {
 
     New-PodeLoggingMethod -Terminal | Enable-PodeErrorLogging -Levels Error,Warning,Informational
 
+    Add-PodeRoute -Method Get -Path '/employee' -ScriptBlock {
+        Write-Host "$(Write-Request -WEvent $WebEvent | Format-List | Out-String)"
+        $Employees = Get-Content -Path ./database/employees.json -RAW | ConvertFrom-Json
+        Write-PodeJsonResponse -StatusCode 200 -Value $Employees
+    }
+
     Add-PodeRoute -Method Post -Path '/employee' -ScriptBlock {
         Write-Host "$(Write-Request -WEvent $WebEvent | Format-List | Out-String)"
-        $Employees = Get-Content ./database/employees.json -Raw
+        $Data = $WebEvent.Request.Body | ConvertFrom-Json
+        Write-Host "$($data | Format-List | Out-String)"
+        $Employees = Get-Content -Path ./database/employees.json | ConvertFrom-Json
+        $Employees.Employee += $Data
+        $Employees | ConvertTo-Json | Out-File ./database/employees.json -Force
         Write-PodeJsonResponse -StatusCode 200 -Value $Employees
     }
 
 }
+    
