3dd3f6112248c7d8ee1eabeb433e17d39f7f7595
Héctor Andrade <andrad_h@hotmail.com>
2022-10-09 10:43:40 -0500

    uses default logging engine instead of custom

--- Pode.GET/src/run.ps1
+++ Pode.GET/src/run.ps1
@@ -10,20 +10,18 @@ Start-PodeServer {
     Add-PodeEndpoint -Address * -Port ($port ? $port : 8080) -Protocol Https -SelfSigned
 
     New-PodeLoggingMethod -Terminal | Enable-PodeErrorLogging -Levels Error,Warning,Informational
+    New-PodeLoggingMethod -Terminal | Enable-PodeRequestLogging
 
     Add-PodeRoute -Method Get -Path '/' -ScriptBlock {
-        Write-Host "$(Write-Request -WEvent $WebEvent | Format-List | Out-String)"
         Write-PodeJsonResponse -StatusCode 200 -Value @{Ready = $True}
     }
 
     Add-PodeRoute -Method Get -Path '/employee' -ScriptBlock {
-        Write-Host "$(Write-Request -WEvent $WebEvent | Format-List | Out-String)"
-        $Employees = Get-Content ./database/employees.json -Raw
+        $Employees = Get-Content ./database/employees.json -Raw | ConvertFrom-Json
         Write-PodeJsonResponse -StatusCode 200 -Value $Employees.Employee
     }
 
     Add-PodeRoute -Method Get -Path '/employee/:id' -ScriptBlock {
-        Write-Host "$(Write-Request -WEvent $WebEvent | Format-List | Out-String)"
         $Employee = ((Get-Content ./database/employees.json -Raw | ConvertFrom-Json).Employee).Where{
                         $_.Id -eq $WebEvent.Parameters['id']
                     }
